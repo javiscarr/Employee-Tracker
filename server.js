f9f7b9b9fd7d6062c74cae9f99912cf47673d6a8
Doonywoony <javismcarr@gmail.com>
2022-01-30 10:30:43 -0500

    inquirer prompt created

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mysql = require('mysql2');
 const inquirer = require('inquirer');
 //importing console.table
 const conTable = require('console.table');
+const { connect } = require('http2');
 
 require('dotenv').config()
 
@@ -14,4 +15,40 @@ const connection = mysql.createConnection({
     password: process.env.MYSQL_PASSWORD,
     database: 'employee_db'
 });
+
+connection.connect(err => {
+    if (err) throw err;
+    console.log('connected as id ' + connection.threadId);
+    
+    beginPrompt();
+});
+
+//first prompt the user will see
+
+const beginPrompt = () => {
+    inquirer.prompt ([
+        {
+            type: 'list',
+            name: 'selection',
+            message: 'What would you like to do?',
+            choices: ['View all departments',
+                      'View all roles',
+                      'View all employees',
+                      'Add a department',
+                      'Add a role',
+                      'Add an employee',
+                      'Update an employee role',
+                      'Update an employee manager',
+                      'View employees by department',
+                      'Delete a department',
+                      'Delete a role',
+                      'Delete an employee',
+                      'View department budgets',
+                      'No action']
+        }            
+                    
+    ])
+
+    
+}
 
